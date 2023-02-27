d7fb133e0c4e623b3ad02c89a5d62682ac0f6c44
Josh Horwitz <jahorwitz94@gmail.com>
2023-02-27 09:09:44 -0500

    initial commit

---
+++ src/routes/home.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import { Home } from "./home";
+
+describe("Route: Home", () => {
+  it("renders headline", () => {
+    render(<Home />);
+
+    expect(screen.getByText("Home")).toBeInTheDocument();
+  });
+});
